30c487194bb7b033c34c4a2f344847598c8a19bf
jorgeagomezphd <59182541+gnumber60@users.noreply.github.com>
2021-07-27 15:43:34 -0500

    define neurotransmitter

--- trace_selection.R
+++ trace_selection.R
@@ -4,13 +4,16 @@
 # Import libraries used
 library(plotly)
 
+# Define the neurotransmitter you are working with
+nt <- readline(prompt = "Enter neurotransmitter being alazyed: ")
+
 #Creating variable to use in ggplotly
 data_plot <- df%>%
   ggplot() +
   geom_line(data = df, aes(x=Time, y = Micromolar), color = "black") +
   theme_bw() +
   theme(panel.grid.major = element_blank(), panel.grid.minor = element_blank()) +
-  labs(x = "Time (s)", y = "Serotonin  (μM)") +
+  labs(x = "Time (s)", y = paste0(nt, " uM")) +
   scale_x_continuous(breaks = seq(min(df$Time), max(df$Time), by = 300))
 
 # Make the graph interactive
